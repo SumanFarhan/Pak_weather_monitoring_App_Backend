26dc796acab2d4d9548606448472bb49720188e5
Aisha Gul <aishagulahmed@gmail.com>
2023-01-21 17:14:21 +0500

    package file

--- router/router.js
+++ router/router.js
@@ -3,7 +3,7 @@ const userController = require('../Controller/userController');
 const weatherController=require('../Controller/Weathercontroller')
 
 
-const API_KEY="c21d24bee643583f8fedd5a633a2db0a"
+
 
 routes.post('/signupUser', userController.addUser);
 routes.post('/loginUser', userController.findUser);
