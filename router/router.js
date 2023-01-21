0fb8f4f976076bc8b4b6eacdc019ba9499fe0ed4
Aisha Gul <aishagulahmed@gmail.com>
2023-01-21 20:14:20 +0500

    jwt router

--- router/router.js
+++ router/router.js
@@ -1,10 +1,11 @@
 const routes = require('express').Router();
 const userController = require('../Controller/userController');
 const weatherController=require('../Controller/Weathercontroller')
+const { authenticate } = require('../Controller/jwtController');
 
 
 
-
+routes.use('/loginUser', authenticate);
 routes.post('/signupUser', userController.addUser);
 routes.post('/loginUser', userController.findUser);
 routes.post('/addweather',weatherController.addweather)
