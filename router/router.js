d054d92f7008bfec650adc54a7bdde14b010ebb4
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2023-01-21 14:31:32 +0500

    Weather Router

--- router/router.js
+++ router/router.js
@@ -1,8 +1,15 @@
 const routes = require('express').Router();
 const userController = require('../Controller/userController');
+const weatherController=require('../Controller/Weathercontroller')
 
+<<<<<<< Updated upstream
 routes.post('/signupUser', userController.addUser);
 routes.post('/loginUser', userController.findUser);
 
+=======
+routes.post('/addUser', userController.addUser);
+// routes.get('/getUser', userController.getUser);
+routes.post('/addweather',weatherController.addweather)
+>>>>>>> Stashed changes
 
 module.exports = routes;
