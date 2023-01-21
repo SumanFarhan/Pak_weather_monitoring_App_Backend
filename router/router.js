07a47701702e69364b271336a29522c13dbd5e5b
Aisha Gul <aishagulahmed@gmail.com>
2023-01-21 13:01:39 +0500

    route to find user upon login

--- router/router.js
+++ router/router.js
@@ -1,8 +1,8 @@
 const routes = require('express').Router();
 const userController = require('../Controller/userController');
 
-routes.post('/addUser', userController.addUser);
-// routes.get('/getUser', userController.getUser);
+routes.post('/signupUser', userController.addUser);
+routes.get('/loginUser', userController.findUser);
 
 
 module.exports = routes;
