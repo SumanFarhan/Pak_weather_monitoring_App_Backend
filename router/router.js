a24d20260ae5c5d2f9a8050bb5200863e6d02613
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2023-01-21 15:24:07 +0500

    Add getweather route

--- router/router.js
+++ router/router.js
@@ -2,8 +2,15 @@ const routes = require('express').Router();
 const userController = require('../Controller/userController');
 const weatherController=require('../Controller/Weathercontroller')
 
+
+const API_KEY="c21d24bee643583f8fedd5a633a2db0a"
+
 routes.post('/signupUser', userController.addUser);
 routes.post('/loginUser', userController.findUser);
 routes.post('/addweather',weatherController.addweather)
+
+routes.get('/weather',weatherController.getWeather);
+
+
 
 module.exports = routes;
