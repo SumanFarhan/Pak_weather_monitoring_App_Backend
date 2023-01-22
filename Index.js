2420d583b5c4227986d26cc3a4f436d24eee1a70
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2023-01-22 12:44:47 +0500

    add socket server

--- Index.js
+++ Index.js
@@ -3,10 +3,11 @@ const app = express();
 const routes = require('./router/router')
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 
 // require('dotenv').config();
-app.use(cors());
-
 try {
     mongoose.connect('mongodb+srv://aisha_Kapadia:Chicken%4098@cluster0.4jnfayp.mongodb.net/WeatherAppDB',
         {
@@ -14,7 +15,7 @@ try {
             useNewUrlParser: true
         }
     )
-    .then(console.log('DB connection'))
+        .then(console.log('DB connection'))
 } catch (error) {
     console.log('Error in DB connection');
 }
@@ -22,10 +23,31 @@ try {
 process.on('UnhandledRejection', error => {
     console.log('DB error', error);
 });
-    
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', routes);
 
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000"
+    }
+});
 
-app.listen(3007);
+
+io.on('connection', (socket) => {
+    console.log(`A user connected:${socket.id}`)
+    socket.on('weather-data', (data) => {
+      io.emit('weather-data', data);
+      console.log('weather data by socket emit')
+    });
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+
+server.listen(3007, () => {
+    console.log('Server running on port 3007');
+});
